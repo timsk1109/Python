ec628381dbd9a04dfcc3885ea1bb97624d5f8d5a
Timothy <timothymang09@gmail.com>
2024-06-10 02:04:23 -0400

    my first commit

---
+++ data_types.py
@@ -0,0 +1,9 @@
+#there are moany data types.
+# string this are words written and closed in quotation marks
+print("this is a new repo")
+# integer these are whole numbers.
+12 334 55
+# boolean this can be either true or false
+True, False
+#float. these are decimal numbers.
+12.2, 4.3
