bffdb87ade836d6bbf0d0743e9a476c75ce3b0bc
timsk1109 <111293099+timsk1109@users.noreply.github.com>
2025-02-20 19:07:31 +0000

    added an if statement

--- test.py
+++ test.py
@@ -2,6 +2,9 @@
 #money_per_hour = 10
 
 def wages(total_hour, money_per_hour):
-    print(f"I made ${total_hour * money_per_hour} weekly as a delivery guy")
+    if money_per_hour > 0:
+      print(f"I made ${total_hour * money_per_hour} weekly as a delivery guy")
+    else:
+       print("you entered an invaild input.")
 money_per_hour = int(input("how much do you make an hour?\n"))
 wages(40, money_per_hour)
